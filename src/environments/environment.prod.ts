dd083c5478493beb53a62f72c71e21bc76811c4d
Hiram weyman <41969577+HiramWeyman@users.noreply.github.com>
2023-06-24 14:03:30 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
 export const environment = {
   production: true,
 
-  //rutaPortal: 'http://192.168.1.190:8080/portalTesoreria/',
-  rutaPortal: 'http://192.168.10.38:8080/portalTesoreria/',
-
-  //rutaAPI: 'http://192.168.1.190:8090/api'
-  //rutaAPI: 'http://192.168.10.38:8090/api'
-  rutaAPI: 'http://200.23.125.18:8090/api'
+  rutaPortal: 'http://localhost:4500/',
+  rutaAPI: 'http://localhost:8090/api'
 };
